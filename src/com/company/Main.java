c681443cdc2d3624919b0df5d2558647dd64c6b1
Dasha Boychuk <dashaboichuk@gmail.com>
2019-08-07 14:07:29 +0300

    homework 1 with tasks

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,24 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        //Tasks
+        //1
+        System.out.println("Hello world");
+        //2
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println("a + b = " + (a + b) + "\na - b = " + (a - b) + "\na * b = " + (a * b) + "\na / b = " + (a / b));
+        //3
+        System.out.println("How are you?");
+        String answer = scanner.next();
+        System.out.println("I am " + answer);
+        //4
+        int x = scanner.nextInt();
+        System.out.println("pow x = " + Math.pow(x, 2) + "\nsqrt x = " + Math.sqrt(x));
+        //Homework
         //1
         System.out.println("Enter radius");
-        Scanner scanner = new Scanner(System.in);
         int radius = scanner.nextInt();
         double perimeter = radius * 2 * Math.PI;
         double area = Math.PI * Math.pow(radius, 2);
