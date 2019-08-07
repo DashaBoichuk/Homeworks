26a191262d1fb29d55858306ba3c2089fcad78ac
Dasha Boychuk <dashaboichuk@gmail.com>
2019-08-07 13:50:49 +0300

    homework 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //1
+        System.out.println("Enter radius");
+        Scanner scanner = new Scanner(System.in);
+        int radius = scanner.nextInt();
+        double perimeter = radius * 2 * Math.PI;
+        double area = Math.PI * Math.pow(radius, 2);
+        System.out.println("perimeter = " + perimeter + "\narea = " + area);
+        //2
+        System.out.println("What is your name?");
+        String name = scanner.next();
+        System.out.println("How old are you?");
+        int age = scanner.nextInt();
+        System.out.println("Name: " + name + "\nAge: " + age);
     }
 }
